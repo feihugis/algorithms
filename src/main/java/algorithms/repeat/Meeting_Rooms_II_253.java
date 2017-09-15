d0aa36282526e7e5c398e17bfb0ed4c77ab7fb0e
Fei Hu <hufei68@gmail.com>
2017-09-15 16:37:22 -0700

    Meeting Rooms_II: this is an interesting question. I think my solution is also interesting.

---
+++ src/main/java/algorithms/repeat/Meeting_Rooms_II_253.java
@@ -0,0 +1,53 @@
+package algorithms.repeat;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by Fei Hu on 9/15/17.
+ */
+
+class Interval {
+  int start;
+  int end;
+  Interval() { start = 0; end = 0; }
+  Interval(int s, int e) { start = s; end = e; }
+}
+
+public class Meeting_Rooms_II_253 {
+
+  public int minMeetingRooms(Interval[] intervals) {
+    Arrays.sort(intervals, new Comparator<Interval>() {
+      @Override
+      public int compare(Interval o1, Interval o2) {
+        return o1.start < o2.start? -1 : 1;
+      }
+    });
+    int count = 0;
+    for (int i = 0; i < intervals.length - 1; i++) {
+      boolean overlap = true;
+      for (int j = 0; j < i; j++) {
+        if ( intervals[i].start >= intervals[j].end) {
+          intervals[j].end = intervals[i].end;
+          intervals[i] = intervals[j];
+          overlap = false;
+          break;
+        }
+      }
+      if (overlap) count++;
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    Meeting_Rooms_II_253 solution = new Meeting_Rooms_II_253();
+    Interval[] input = new Interval[3];
+    input[0] = new Interval(5, 10);
+    input[1] = new Interval(0, 30);
+    input[2] = new Interval(15, 20);
+    System.out.println(solution.minMeetingRooms(input));
+  }
+
+
+
+}
