6a69a94a11f01b5e35f5258f84cb0b0aa802524b
Fei Hu <hufei68@gmail.com>
2017-09-15 23:21:03 -0700

    Finally get better understanding of this type of problems

---
+++ src/main/java/algorithms/done/Game_of_Life_289.java
@@ -0,0 +1,8 @@
+package algorithms.done;
+
+/**
+ * Created by Fei Hu on 9/15/17.
+ */
+public class Game_of_Life_289 {
+
+}
---
+++ src/main/java/algorithms/repeat/Find_the_Duplicate_Number_287.java
@@ -0,0 +1,8 @@
+package algorithms.repeat;
+
+/**
+ * Created by Fei Hu on 9/15/17.
+ */
+public class Find_the_Duplicate_Number_287 {
+
+}
